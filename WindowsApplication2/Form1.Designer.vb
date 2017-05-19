55de413fdb505989234e8d4326cd0780a255784e
user <user@YH-20170224NRDM>
2017-05-20 04:32:16 +0800

    添加项目文件。

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(40, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(2, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(17, 12)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "IP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(2, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Port"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(40, 47)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 10
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(40, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "连接"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(115, 230)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "输入数据"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(174, 227)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(283, 21)
+        Me.TextBox3.TabIndex = 14
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(174, 273)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "发送"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(174, 12)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox4.Size = New System.Drawing.Size(283, 183)
+        Me.TextBox4.TabIndex = 16
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(40, 119)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "断开"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(473, 12)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(17, 12)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "ap"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(473, 50)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(17, 12)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "ad"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(520, 9)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox5.TabIndex = 20
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(521, 47)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox6.TabIndex = 21
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(642, 387)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "自制"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,187 @@
+﻿Public Class Form1
+
+
+
+    Public socketi As Net.Sockets.Socket
+    Dim Th As Threading.Thread
+
+    Public Sub Task()
+
+
+        While 1
+            Dim str As New Text.StringBuilder
+            Dim str2 As String = Nothing
+            Dim str3 As String = Nothing
+            Dim by(100) As Byte
+            Dim ch(100) As Char
+            Dim ch1(100) As Char
+            Dim err As Int32 = socketi.Receive(by)
+            If err > 0 Then
+                'str = BitConverter.ToString(by)
+                '///////////////////////////////////
+                'Dim pos As Int32 = 0
+                'Dim first As Int32 = 0
+                'Dim second As Int32 = 0
+                'Dim SSize As Int32 = str.Length
+                'While second <= SSize And first <= SSize And str.Chars(first) <> "0"c And str.Chars(second) <> "0"c
+                '    ch(pos) = str.Chars(first)
+                '    pos = pos + 1
+                '    ch(pos) = str.Chars(second)
+                '    pos = pos + 1
+                '    ch(pos) = " "c
+                '    pos = pos + 1
+                '    first += 3
+                '    second += 3
+                'End While
+
+                str.Append(System.Text.ASCIIEncoding.ASCII.GetString(by, 0, by.Length()))
+                '////////////////////////////////
+                Dim str1 As String = TextBox4.Text
+                str3 = str.ToString()
+                TextBox4.Text = str1 & str.ToString()
+                str1 = TextBox4.Text
+                TextBox4.Text = str1 & vbNewLine
+                '/////////////////////////////////
+                Dim size As Int32 = str3.Length
+                Dim i As Int32 = 0
+                Dim j As Int32 = 0
+                While i < size；
+                    If str3.Chars(i) = "="c Then
+                        Exit While
+                    End If
+                    i = i + 1
+                End While
+                i += 1
+                While i < size
+                    If str3.Chars(i) = " "c Then
+                        Exit While
+                    End If
+                    ch(j) = str3.Chars(i)
+                    j += 1
+                    i += 1
+                End While
+                '///////////////////////
+                j = 0
+                While i < size
+                    If str3.Chars(i) = "="c Then
+                        Exit While
+                    End If
+                    i = i + 1
+                End While
+                i += 1
+                While i < size
+                    ch1(j) = str3.Chars(i)
+                    j += 1
+                    i += 1
+                End While
+                Dim text1 As String = ch
+                Dim text2 As String = ch1
+                TextBox5.Text = text1
+                TextBox6.Text = text2
+            End If
+        End While
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim flag As Int32 = 1
+        Dim Ip As String = Nothing
+        Dim Port As String = Nothing
+        Ip = TextBox1.Text
+        While Ip Is ""
+            MsgBox("请输入Ip地址", MsgBoxStyle.OkOnly, Me.Text)
+            Return
+        End While
+
+        Port = TextBox2.Text
+        While Port Is ""
+            MsgBox("请输入Port地址", MsgBoxStyle.OkOnly, Me.Text)
+            Return
+        End While
+        '///////////////////////////////////////////////////////////链接代码
+        Dim LocalEndPoint As New Net.IPEndPoint(Net.IPAddress.Parse(Ip), CInt(Port))
+
+        Dim client As Net.Sockets.Socket = Nothing
+        client = New Net.Sockets.Socket(Net.Sockets.AddressFamily.InterNetwork, Net.Sockets.SocketType.Stream, Net.Sockets.ProtocolType.Tcp)
+
+        client.Connect(LocalEndPoint)
+        socketi = client
+        ' This is how you can determine whether a socket is still connected.
+        Dim blockingState As Boolean = client.Blocking
+        Try
+            Dim tmp(0) As Byte
+
+            client.Blocking = False
+            client.Send(tmp, 0, 0)
+            MsgBox("Connected!")
+        Catch ie As Net.Sockets.SocketException
+            ' 10035 == WSAEWOULDBLOCK
+            flag = 0
+            If ie.NativeErrorCode.Equals(10035) Then
+                MsgBox("Still Connected, but the Send would block")
+            Else
+                MsgBox("Disconnected: error code {0}!", ie.NativeErrorCode)
+            End If
+
+        Finally
+            client.Blocking = blockingState
+        End Try
+
+        MsgBox("Connected: {0}", client.Connected)
+        If flag <> 1 Then
+            Return
+        End If
+        Dim newTheread As Threading.Thread = New Threading.Thread(AddressOf Task)
+        Th = newTheread
+        newTheread.Start()
+
+
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim str As String = Nothing
+        str = TextBox3.Text
+        If str Is "" Then
+            MsgBox("请输入数据", MsgBoxStyle.Information)
+            Return
+        End If
+        Dim encText As New System.Text.UTF8Encoding()
+        Dim btText() As Byte
+        btText = encText.GetBytes(str)
+        socketi.Send(btText)
+        Dim str1 As String = TextBox4.Text
+        TextBox4.Text = str1 & str & vbNewLine
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        socketi.Close()
+        Try
+            Th.Abort()
+
+        Catch ex As Exception
+            Return
+        End Try
+
+        MsgBox("连接断开")
+    End Sub
+
+
+End Class
